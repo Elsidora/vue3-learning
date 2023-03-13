08b54a52c9403f73e546250f27012ec5c58c9a9e
ella <ellasidorova@yandex.ru>
2023-03-13 22:35:36 +0300

    двустороннее связывание

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const App = {
   data() {
     return {
       placeholderString: 'Введите название заметки',
-      title: 'Список заметок'
+      title: 'Список заметок',
+      inputValue: ''
+    }
+  },
+  methods: {
+    changeInputHandler(evt) {
+      console.log('changeInputHandler', evt.target.value)
+      this.inputValue=evt.target.value
     }
   }
 }
